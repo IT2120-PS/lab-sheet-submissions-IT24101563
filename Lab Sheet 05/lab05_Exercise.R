435e8abbf9acb15b92b29ee113618c1a9aa95f71
IT24101563 <it24101563@my.sliit.lk>
2025-08-26 20:39:06 +0530

    Update lab05_Exercise.R

--- Lab Sheet 05/lab05_Exercise.R
+++ Lab Sheet 05/lab05_Exercise.R
@@ -1,46 +1,35 @@
-#Q1
 setwd("C:\\Users\\it24101563\\Desktop\\IT24101563")
-data <- read.table("Exercise - Lab 05.txt", header=TRUE, sep=",")
-fix(data)
-attach(data)
+DeliveryTimes <- read.table("Exercise - Lab 05.txt", header=FALSE)
+colnames(DeliveryTimes) <- c("time")   # rename column
+attach(DeliveryTimes)
 
-#Q2
-names(data)<-c("X1")
-attach(data)
-histrogram <- hist(
-  X1,
-  main = "Histogram for Deliver times", 
-  breaks = seq(20, 70, length=10), 
-  right = FALSE)
+histogram <- hist(
+  time,
+  main = "Histogram for Delivery Times",
+  xlab = "Delivery Time",
+  ylab = "Frequency",
+  breaks = seq(20, 70, length.out = 10),  # 9 classes
+  right = FALSE,
+  col = "lightblue",
+  border = "black"
+)
 
-#Q3
-#
+# Q3: Comment on distribution shape
+# The histogram shows that the distribution of delivery times is 
+# approximately symmetric but slightly skewed to the right. 
+# Most delivery times are clustered in the middle class intervals,
+# with fewer values at the extreme ends.
 
-#Q4
-breaks <- round(histrogram$breaks)
-breaks
-freq <- histrogram$counts
-freq
-mids <- histrogram$mids
-mids
+breaks <- histogram$breaks
+freq <- histogram$counts
+cum.freq <- c(0, cumsum(freq))  
 
-cum.freq <- cumsum(freq)
+plot(breaks, cum.freq,
+     type = "l",
+     main = "Cumulative Frequency Polygon (Ogive)",
+     xlab = "Delivery Time",
+     ylab = "Cumulative Frequency",
+     col = "blue",
+     lwd = 2)
 
-new <- c()
-
-for(i in 1:length(breaks)){
-  if(i==1){
-    new[i] = 0
-  }else{
-    new[i] = cum.freq[i-1]
-  }
-}
-
-plot(breaks, new , 
-     type = "l", 
-     main = "Cumalative Frequency Polygon for deliver times", 
-     xlab = "Shareholders", 
-     ylab = "Cumulative Frequncy", 
-     ylim = c(0,max(cum.freq)))
-
-cbind(Upper = breaks, CumFreq = new)
+cbind(Upper = breaks, CumFreq = cum.freq)
